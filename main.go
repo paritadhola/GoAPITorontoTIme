836e86dc2fe6a4235deee09b69f9ae7b047328b3
Parita Dhola <dholaparita@gmail.com>
2024-11-28 16:11:16 -0500

    modified docker-compose.yml

--- main.go
+++ main.go
@@ -10,9 +10,10 @@ import (
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
-// Structure for store time response in jason format
+// Structure for store time response in JSON format
 type TimeResponse struct {
 	CurrentTime string `json:"current_time"`
 	Location    string `json:"location"`
@@ -20,28 +21,40 @@ type TimeResponse struct {
 
 var db *sql.DB
 
+// Function to connect to MySQL database
 func connectDB() {
 	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
 	dbHost := os.Getenv("DB_HOST")
 
+	// Connection string format
 	dbSrcName := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", dbUser, dbPassword, dbHost, dbName)
+
+	// Using backoff to retry DB connection
+
 	db, err = sql.Open("mysql", dbSrcName)
 	if err != nil {
-		log.Fatalf("Unable to connect to database: %v", err)
+		log.Printf("Unable to connect to database: %v", err)
+
 	}
 
+	// Ping to test the connection
 	if err = db.Ping(); err != nil {
-		log.Fatalf("Database ping failed: %v", err)
+		log.Printf("Database ping failed: %v", err)
+
 	}
 	fmt.Println("Database connected successfully!")
 
 }
 
 func getTimeZoneHandler(w http.ResponseWriter, r *http.Request) {
-
 	loc, err := time.LoadLocation("America/Toronto") // Static input for Toronto time
 	if err != nil {
 		http.Error(w, "Error loading timezone", http.StatusBadRequest)
@@ -53,13 +66,14 @@ func getTimeZoneHandler(w http.ResponseWriter, r *http.Request) {
 	formattedTime := currentTime.Format("2006-01-02 15:04:05")
 	log.Printf("Current Toronto time: %s", formattedTime)
 
+	// Insert current time into the database
 	_, err = db.Exec("INSERT INTO time_log (timestamp) VALUES (?)", formattedTime)
 	if err != nil {
 		log.Fatalf("Failed to execute query: %v", err)
 	}
 	log.Println("Data inserted successfully!")
 
-	// Get the response
+	// Prepare and send the response
 	response := TimeResponse{
 		CurrentTime: formattedTime,
 		Location:    "Toronto",
@@ -72,28 +86,13 @@ func getTimeZoneHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// Connect to DB
+	connectDB()
 
-	dbSrcName := "root:12345678@tcp(localhost:3306)/GoTimeZoneAPI"
-	db, err := sql.Open("mysql", dbSrcName)
-	if err != nil {
-		log.Fatalf("Unable to connect to database: %v", err)
-	}
-	defer db.Close()
-
-	if err = db.Ping(); err != nil {
-		log.Fatalf("Database ping failed: %v", err)
-	}
-
-	_, err = db.Exec("INSERT INTO time_log (timestamp) VALUES (NOW())")
-	if err != nil {
-		log.Fatalf("Failed to execute query: %v", err)
-	}
-	log.Println("Data inserted successfully!")
-
-	// Start the server for callling API
+	// Start the server for calling the API
 	http.HandleFunc("/time", getTimeZoneHandler)
 
 	port := ":8080"
-	println("Server is running on port", port)
+	fmt.Println("Server is running on port", port)
 	http.ListenAndServe(port, nil)
 }
